975c51a4eff5477f5ed0e7ea2362df6cebce4575
Shaahid Adams <shaahid.adams@gmail.com>
2024-09-27 02:32:30 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Sections/Navbar'
+import Hero from './Sections/Hero'
+
+
+
+const App = () => {
+    
+    return (
+    <main className="max-w-7xl mx-auto">
+      <Navbar/>
+      <Hero/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/Sections/Hero.jsx
@@ -0,0 +1,18 @@
+
+const Hero = () => {
+  return (
+    <section className="min-h-screen w-full flex flex-col relative">
+      <div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+        <p className="sm:text-3xl text-xl font-medium text-center text-white font-generalsans"> 
+            Hi I'm Shaahid
+            <span className="waving-hand">🫡</span>
+        </p>
+        <p className="hero_tag text-gray_gradient">
+            Software Developer and Computer Wizard
+        </p>
+      </div>
+    </section>
+  )
+}
+
+export default Hero
